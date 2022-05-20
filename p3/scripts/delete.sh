484f2c619d434d005bd119b24c4a6bf7149f0e4f
denis g <gavrilin94@mail.ru>
2022-05-21 01:53:54 +0300

    trying to complete p3

--- p3/scripts/delete.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# cluster
-k3d cluster delete mycluster
--- p3/scripts/password.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
