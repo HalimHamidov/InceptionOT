d3b289a641052272c7a2790e1e28d1fcc1d5714f
denis g <gavrilin94@mail.ru>
2022-05-27 23:26:25 +0300

    p3 works

--- p3/scripts/deploy.sh
+++ p3/scripts/deploy.sh
@@ -1,65 +1,40 @@
-CONFIG_PATH="/vagrant"
+#!/bin/bash
 
-## [NAMESPACES] : create {argocd, dev} namespaces as required in the subjcet
-echo "[NAMESPACES] : create {argocd, dev} namespaces as required in the subjcet"
+# install docker
+apt-get update
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io
+
+# install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+# install k3d
+wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+
+
+# create cluster
+k3d cluster create gjigglypCluster -p "8888:30080"
+
+# create namespaces
 kubectl create namespace argocd
 kubectl create namespace dev
 
-
-## [ARGOCD] : install argocd and wait for pods to rollout...
-echo "################################################################################"
-echo "[ARGOCD] : install argocd and wait for pods to rollout..."
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
-sudo kubectl apply -f $CONFIG_PATH/confs/argoInstall.yaml -n argocd
-# the command is downloading the config file only and then run the installation in the backend
-# so you need to wait untill all the pods are running and ready
-# watch kubectl get pods -n argocd
-# kubectl wait --for=condition=Ready --timeout=-1s  pods --all -n argocd
-
-# [ARGOCD-INGRESS] : setup ingress for argocd so we can access it from 8000
-echo "################################################################################"
-echo "[ARGOCD-INGRESS] : setup ingress for argocd to we can access it from 8000"
-kubectl apply -n argocd -f $CONFIG_PATH/confs/ingress.yaml
-
-# [ARGOCD-ROLLOUT] : wait for argocd pods to be running
-echo "################################################################################"
-echo "[ARGOCD-ROLLOUT] : wait for argocd pods to be running"
-kubectl rollout status deployment argocd-server -n argocd
-kubectl rollout status deployment argocd-redis -n argocd
-kubectl rollout status deployment argocd-repo-server -n argocd
-kubectl rollout status deployment argocd-dex-server -n argocd
-
-
-# [ARGOCD-PROJECT-DEVELOPMENT] : Let us create a dedicated ArgoCD project named development 
-#                               and let us voluntarily put some restrictions in this project, 
-#                               that will apply to all applications 
-#                               that inside this project: allow only in-cluster deployments in the dev namespace.
-# echo "################################################################################"
-# echo "[ARGOCD-PROJECT-DEVELOPMENT] : setup project rules..."
-# sudo kubectl apply -f $CONFIG_PATH/confs/project.yaml -n argocd
-
-# [wils-APPLICATION] : setup wils application to fetch its config from our github repo
-echo "################################################################################"
-echo "[wils-APPLICATION] : setup wils application to fetch its config from our github repo"
-kubectl apply -n argocd -f $CONFIG_PATH/confs/wils-Application.yaml
-
-# [ARGOCD-ADMIN-PASSWORD] : retrieve the password for the admin user of the dashboard
-echo "################################################################################"
-echo "[ARGOCD-ADMIN-PASSWORD] : retrieve the password for the admin user of the dashboard"
-echo "YOUR PASSWORD IS ==============>"
-echo "========================================================================>"
-echo "========================================================================>"
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-echo "<======================================================================="
-echo "<======================================================================="
-
-sudo kubectl wait --for=condition=Ready pods --all -n argocd
-sleep 20
-echo "[WILL-APP] : test app at http://localhost:8888/"
-curl http://localhost:8888/
-
-echo "############################################################################"
-echo "############################## ALL SET !! ##################################"
-echo "######################### ARGOCD : localhost:8080 ##########################"
-echo "######################### WIL-APP: localhost:8888 ##########################"
-echo "############################################################################"
+# setup argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
