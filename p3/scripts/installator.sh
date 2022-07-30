0ea9a5ffa1c71fd5435f1680748e85e2a207db71
Greenwheat Jigglypuff <gjigglyp@at-d1.msk.21-school.ru>
2022-07-30 16:44:03 +0300

    p3 is checking again

--- p3/scripts/installator.sh
+++ p3/scripts/installator.sh
@@ -1,39 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-sudo cat /home/vagrant/.ssh/id_rsa.pub  >> /home/vagrant/.ssh/authorized_keys
-sudo mkdir /root/.ssh
-sudo cp -R  /home/vagrant/.ssh/*  /root/.ssh
+# cluster
+k3d cluster create mycluster --servers 1 --agents 0 -p "8080:80@loadbalancer" -p "8888:8888@loadbalancer"
 
-echo "скачиваем и запускаем докер"
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh ./get-docker.sh
-sudo usermod -aG docker $USER
-sudo service docker start
-sudo chmod 666 /var/run/docker.sock
-
-echo "Устанавливаем kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-echo "Устанавливаем k3d"
-curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
-k3d cluster create mycluster --port 8888:8888@loadbalancer 
-
-#echo "Устанавливаем AgroCD в класстер k3d"
+# namespace
 kubectl create namespace argocd
 kubectl create namespace dev
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl -n argocd patch svc/argocd-server -p '{"spec": {"type": "LoadBalancer"}}'
-kubectl apply -n argocd -f /home/vagrant/confs/argocd.yaml
-kubectl wait --for=condition=available deployment --all -n argocd --timeout=3m
-kubectl wait --for=condition=ready pod --all -n argocd --timeout=3m
-#echo "Парроль ArgoCD"
-echo `kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath={.data.password} | base64 --decode`
 
-#После запуска ВМ можно подключится и пробрасить порты
-kubectl port-forward svc/argocd-server -n argocd 8443:443 --address=0.0.0.0&
-
-# После запуска:
-# 1. повторно пробросить порт для argocd: kubectl port-forward svc/argocd-server -n argocd 8443:443 --address=0.0.0.0&
-# 2. дождаться, когда приложение развернется
-# 3. Проверить приложение: curl http://localhost:8888
+# for an IngressRoute type
+curl https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml | sed -e 's/\( *\)- argocd-server/&\n\1- --insecure/' | kubectl apply -n argocd -f -
+sleep 3
+kubectl apply -f /vagrant/confs/ingress.yaml
+kubectl apply -f /vagrant/confs/argo.yaml
